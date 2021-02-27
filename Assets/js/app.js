604da1006c2d542964f1a8905751919b1470a7ff
Bontu <se.bontu.fufa@gmail.com>
2021-02-27 15:05:43 +0300

    spiner and js first integration

---
+++ Assets/js/app.js
@@ -0,0 +1,24 @@
+let apiBox = document.querySelector('#api_box')
+
+let pageLoader = document.querySelector('.spin');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchWanted();
+})
+
+function fetchWanted() {
+    // fetch(' https://cors-anywhere.herkuapp.com/https://api.fbi.gov/wanted/v1/list')
+    fetch('http://api.fbi.gov/wanted/v1/list')
+    .then(function(res) {  return res.json(); //return the JSON Promise
+    })
+    .then(function(posts) {
+        //iterate over each post [100 posts]
+        let output = 'success fully fetched '
+        // ${posts[title]} ;
+        postDiv3.innerHTML = output;
+    })
+    .catch(function(err) {     
+        console.log(err);
+    });
+}
