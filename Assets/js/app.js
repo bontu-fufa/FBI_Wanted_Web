d4ee61a8b0ab405fcfea5cb8f741311e22f401a1
hanna kebede <se.hanna.kebede@gmail.com>
2021-03-02 21:06:07 +0300

    filter function

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -36,6 +36,101 @@ const fetchJson = async function(){
 
 
 // filter crime
+var cyber = document.querySelector("#Cybers_Most_Wanted");
+var murders = document.querySelector("#Violent_Crime_Murders");
+var seeking = document.querySelector('#Seeking_Information')
+var white = document.querySelector('#White-Collar_Crime')
+var kidnapping = document.querySelector('#Kidnappings_and_Missing_Persons')
+var vicap = document.querySelector('#ViCAP_Unidentified_Persons')
+var additional = document.querySelector('#Additional_Violent_Crimes')
+
+
+var filter_crime = function(choosen){
+  fetchJson().then(jsonData =>{
+    displayBox.innerHTML= '';
+    var items = jsonData.items
+    var crime = choosen.replaceAll("_"," ")
+    log(items)
+    log(crime)
+
+    var results = items.filter(item => item.subjects == crime )
+    var total_crime = results.length;
+    log(results)
+    var html = ''
+    results.forEach(item => {
+            log(item)
+            var images = item.images
+            var imagesUrl = images[0].original
+            // name - Title
+            var name = item.title
+
+            // files - english will be filtered 
+            var files = item.files 
+            var firstFileUrl = files[0].url
+
+            // reward text 
+            var rewardTxt = !(item.reward_text === null) ? item.reward_text : 'reward not mentioned'
+            
+            // description
+            var description = item.description
+            
+            // subject
+            var subjects = item.subjects.length > 0 ? (item.subjects[0]) :  'subjects not mentioned'
+            
+            // uid 
+            var uidid = item.uid 
+            // modal id 
+            var idd = `#${uidid}`
+
+
+
+            html += `<div class="col-12 my-3">
+            <div class="card ">
+                <div class="card-horizontal row">
+                    
+                    <div class="card-body col-md-9">
+                        
+                        <div class="d-flex justify-content-between">
+                            <h4 class="card-title text-center color-blueblack"> ${name}</h4>
+                            <button type="button" class=" btn  btn-outline border-0" data-toggle="modal" data-target="${idd}" >
+                         <i class="fa fa-info-circle" aria-hidden="true" ></i></button>
+                        </div>
+                        <p><small>Subjects </small></p>
+                        <p class="card-text color-black">
+                        ${subjects}
+                        </p>
+
+                        <p><Small>Reward</Small></p>
+                        <p class="card-text color-black "> <em>
+                        ${rewardTxt}
+                        </em> </p>
+                        <button class="btn btn-fill-1 mt-2" style="width:100%"><i class="fa fa-download"></i>
+                        <a href="${firstFileUrl}" class="color-white" download="download.pdf"> Download File</a>
+                        </button>
+
+                    </div>
+                    <div class="img-square-wrapper col-md-3 ">
+                        <img class="img-adjust" src="${imagesUrl}" alt="Card image cap">
+                    </div>
+                </div>
+                </div>
+                </div>
+
+               
+          `
+
+      
+    })
+
+
+    html += (total_crime > 1) ? <p>${total_crime} results are found! </p> : <p>${total_crime} result is found! </p> 
+
+    log(html)
+    displayBox.innerHTML = html;
+
+})
+  
+}
 
 
 
